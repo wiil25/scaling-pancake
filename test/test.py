5d96f8b17e106764fe4c39b253e1920838148dc1
WILSON LEZ DA SILVA <wilson250792@gmail.com>
2024-09-28 20:21:41 -0300

    atualizando pr

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.main import *
 from unittest.mock import patch
 
@@ -15,7 +17,7 @@ def test_funcaoteste():
 
 def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
-    assert estudante_teste == create_estudante()
+    assert estudante_teste == create_estudante(estudante_teste)
 
 
 def test_update_estudante_negativo():
